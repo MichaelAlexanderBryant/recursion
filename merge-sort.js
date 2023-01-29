6f69800185a983e4c5a959afcd717abb85e741e2
MichaelABryant <m.alexander.bryant@gmail.com>
2023-01-29 11:52:57 -0800

    Add merge sort function

---
+++ merge-sort.js
@@ -0,0 +1,33 @@
+function mergeSort(arr) {
+    if (arr.length < 2) {
+        return arr;
+    };
+    let half = Math.ceil(arr.length / 2);
+    let left = arr.slice(0, half);
+    let right = arr.slice(half);
+    return mergeHelper(mergeSort(left), mergeSort(right))
+
+};
+
+function mergeHelper(a, b) {
+    let sortedArr = [];
+    while ((a.length > 0) && (b.length > 0)) {
+        if (a[0] <= b[0]) {
+            sortedArr.push(a.shift());
+        } else {
+            sortedArr.push(b.shift())
+        };
+    };
+
+    if (a.length > 0) {
+        sortedArr = sortedArr.concat(a);
+    } else {
+        sortedArr = sortedArr.concat(b);
+    };
+    return sortedArr;
+};
+
+console.log(mergeSort([2,1,3]));
+console.log(mergeSort([2,1,1,1,3]));
+console.log(mergeSort([2,1,3,10,0,-3]));
+console.log(mergeSort([-2,1,3,0,0,0,-55, 10000]));
